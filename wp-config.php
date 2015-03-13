d8af2eade7d2732ffe0c637e92ab0644816d7f7b
jamiefinlayson <jamie@bright-interactive.co.uk>
2015-03-13 13:26:24 +0000

    Added password for local and production db

--- wp-config.php
+++ wp-config.php
@@ -30,13 +30,13 @@ else {
 if (WP_ENV == 'development') {
     define('DB_NAME', 'mydb-dev');
     define('DB_USER', 'root');
-    define('DB_PASSWORD', '');
+    define('DB_PASSWORD', 'root');
     define('DB_HOST', 'localhost');
 } 
 else if (WP_ENV == 'production') {
     define('DB_NAME', 'mydb-prod');
     define('DB_USER', 'root');
-    define('DB_PASSWORD', '');
+    define('DB_PASSWORD', 'root');
     define('DB_HOST', 'localhost');
 } 
 
