5063f12f57b9e347742488f15dd723661fe5c614
jamiefinlayson <jamie@bright-interactive.co.uk>
2015-03-29 17:28:41 +0100

    styled search results screen

--- wp-content/themes/dpp/search.php
+++ wp-content/themes/dpp/search.php
@@ -7,15 +7,16 @@
 			<div class="small-12 medium-12 large-12 xlarge-12 columns">
 				<h2>Search Results</h2>
 			</div>
-		</div>
+	 
 
 		<?php include (TEMPLATEPATH . '/includes/inc/nav.php' ); ?>
 
+		<div class="small-12 medium-12 large-9 xlarge-9 columns">
 		<?php while (have_posts()) : the_post(); ?>
 
 			<article <?php post_class() ?> id="post-<?php the_ID(); ?>">
 
-				<h2><?php the_title(); ?></h2>
+				<h2><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h2>
 
 				<?php include (TEMPLATEPATH . '/includes/inc/meta.php' ); ?>
 
@@ -30,6 +31,8 @@
 		<?php endwhile; ?>
 
 		<?php include (TEMPLATEPATH . '/includes/inc/nav.php' ); ?>
+	
+	</div> 	
 
 	<?php else : ?>
 
@@ -41,7 +44,12 @@
 
 	<?php endif; ?>
 
-</div>
-<?php get_sidebar(); ?>
+<div class="small-12 medium-12 large-3 xlarge-3 columns">
+				<aside >
+					<?php get_sidebar(); ?>
+				</aside>
+			</div>
 
+</div>
+</div>
 <?php get_footer(); ?>
