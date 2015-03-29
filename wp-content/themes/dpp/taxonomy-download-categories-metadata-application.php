3c168f9d52e2f3b7e3178bfe6ab324fe66f4aa25
jamiefinlayson <jamie@bright-interactive.co.uk>
2015-03-29 19:33:15 +0100

    Added attachment to downloads single pages

--- wp-content/themes/dpp/taxonomy-download-categories-metadata-application.php
+++ wp-content/themes/dpp/taxonomy-download-categories-metadata-application.php
@@ -61,8 +61,8 @@
 					<?php if (have_posts()) : ?>
 
 								<?php while (have_posts()) : the_post(); ?>
-
-										<?php the_content();?>
+ 
+									<?php get_template_part( 'includes/inc/content', 'block-side-download'); ?>					
 
 								<?php endwhile; ?>
 
