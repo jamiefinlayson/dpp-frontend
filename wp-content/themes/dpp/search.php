d72a4ac32c9d192562f8d6d018ee7ef3723aad34
jamiefinlayson <jamie@bright-interactive.co.uk>
2015-04-05 00:27:00 +0100

    Modified search results page layout to fit design

--- wp-content/themes/dpp/search.php
+++ wp-content/themes/dpp/search.php
@@ -1,55 +1,53 @@
 <?php get_header(); ?>
 <div class="primary-content search-results">
-
-	<?php if (have_posts()) : ?>
-
-		<div class="row">
+<div class="row">
 			<div class="small-12 medium-12 large-12 xlarge-12 columns">
 				<h2>Search Results</h2>
 			</div>
-	 
+	<?php if (have_posts()) : ?>
 
-		<?php include (TEMPLATEPATH . '/includes/inc/nav.php' ); ?>
+		
 
-		<div class="small-12 medium-12 large-9 xlarge-9 columns">
-		<?php while (have_posts()) : the_post(); ?>
 
-			<article <?php post_class() ?> id="post-<?php the_ID(); ?>">
+			<?php include (TEMPLATEPATH . '/includes/inc/nav.php' ); ?>
 
-				<h2><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h2>
+			<div class="small-12 medium-12 large-9 xlarge-9 columns">
+				<?php while (have_posts()) : the_post(); ?>
 
-				<?php include (TEMPLATEPATH . '/includes/inc/meta.php' ); ?>
+					<article <?php post_class() ?> id="post-<?php the_ID(); ?>">
 
-				<div class="entry">
+						<h2><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h2>
 
-					<?php the_excerpt(); ?>
+						<?php include (TEMPLATEPATH . '/includes/inc/meta.php' ); ?>
 
-				</div>
+						<div class="entry">
 
-			</article>
+							<?php the_excerpt(); ?>
 
-		<?php endwhile; ?>
+						</div>
 
-		<?php include (TEMPLATEPATH . '/includes/inc/nav.php' ); ?>
-	
-	</div> 	
+					</article>
 
-	<?php else : ?>
+				<?php endwhile; ?>
 
-		<div class="row">
-			<div class="small-12 medium-12 large-12 xlarge-12 columns">
+				<?php include (TEMPLATEPATH . '/includes/inc/nav.php' ); ?>
+
+			</div> 	
+
+		<?php else : ?>
+
+			<div class="small-12 medium-12 large-9 xlarge-9 columns">
 				<h2>No results</h2>
-			</div>
+			</div> 
+
+		<?php endif; ?>
+
+		<div class="small-12 medium-12 large-3 xlarge-3 columns">
+			<aside >
+				<?php get_sidebar(); ?>
+			</aside>
 		</div>
 
-	<?php endif; ?>
-
-<div class="small-12 medium-12 large-3 xlarge-3 columns">
-				<aside >
-					<?php get_sidebar(); ?>
-				</aside>
-			</div>
-
-</div>
+	</div>
 </div>
 <?php get_footer(); ?>
