0b0435ce244d887964cb99e484fd06152a046874
jamiefinlayson <jamie@bright-interactive.co.uk>
2015-04-09 20:26:11 +0100

    Styled events layout

--- wp-content/themes/dpp/includes/inc/content-block-event.php
+++ wp-content/themes/dpp/includes/inc/content-block-event.php
@@ -1,14 +1,14 @@
 <div class="block-content thumbnail-on-left outlined-box for-news">
 	<article <?php post_class() ?> id="post-<?php the_ID(); ?>">
 
-		<div class="thumbnail-wrap inner small-5 medium-12 columns">
+		<div class="thumbnail-wrap">
 		<?php if ( has_post_thumbnail() ) { ?>
 			<a href="<?php the_permalink() ?>"> <?php the_post_thumbnail( 'thumbnail',array( 'class'	=> "highlight-bottom")); ?></a>
 		<?php } else { ?>
 			<a href="<?php the_permalink() ?>"> <img src="<?php bloginfo('template_directory');?>/common/img/thumbnail-default.jpg" /></a>
 		<?php } ?>
 		</div>
-		<div class="padded-content inner small-7 medium-12 columns">
+		<div class="padded-content">
 			<header>
 				<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
 
@@ -29,7 +29,7 @@
 			</header>
 
 		</div>
-		<div class="padded-text inner small-12 medium-12 columns">
+		<div class="padded-text inner">
 			<p><?php echo excerpt(150); ?></p>
 			<a class="more-link chevron-before" href="<?php the_permalink();?>"><span><?php the_field('custom-readmore'); ?></span></a>
 		</div>
